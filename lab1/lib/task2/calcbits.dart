4795f606d9b61355cd32e43d58022272dc215017
karin0chkaa <malininakarina172@gmail.com>
2025-02-26 19:50:35 +0300

    Main has been changed

--- lab1/lib/task2/calcbits.dart
+++ lab1/lib/task2/calcbits.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 class BitCounter {
-  static const String invalidMessage = 'Ошибка: введено некорректное число!';
+  static const String invalidMessage = 'Error: Incorrect number entered';
 
-  int countingNumberOfBits(int number) {
+  int countNumberOfBits(int number) {
     int count = 0;
     while (number != 0) {
       count += number & 1;
@@ -12,14 +12,14 @@ class BitCounter {
     return count;
   }
 
-  void performingTheCalculation(String inputNumber) {
+  void performCalculation(String inputNumber) {
     if (int.tryParse(inputNumber) == null || int.parse(inputNumber) < 0 || int.parse(inputNumber) > 255) {
       throw FormatException(BitCounter.invalidMessage);
     }
 
     int decNumber = int.parse(inputNumber);
-    int countOfBits = countingNumberOfBits(decNumber);
+    int countOfBits = countNumberOfBits(decNumber);
 
-    print('Количество единичных битов = ${countOfBits}');
+    print('Number of single bits = ${countOfBits}');
   }
 }
--- lab1/lib/task2/main.dart
+++ lab1/lib/task2/main.dart
@@ -1,40 +1,17 @@
 import 'dart:io';
+import 'package:args/command_runner.dart';
+import 'count_bits_command.dart';
 import 'calcbits.dart';
 
-const String invalidArgumentMessage = 'Invalid argument';
-const String inputText = 'Введите десятичное число от 0 до 255: ';
-const String helpMessage = '''
-Справка об использовании программы: 
-Необходимо использовать: calcbits.exe <byte>
-Программа подсчитывает количество единичных битов в байте.
-При запуске без параметров командной строки программа считывает число (байт) из stdin.
-Параметр "-h" показывает справку об использовании программы.
-''';
-
 void main(List<String> arguments) {
-  if(arguments.isEmpty) {
-    stdout.write(inputText);
-    String? number = stdin.readLineSync();
+  final runner = CommandRunner('calcbits', 'A program for counting single bits in a byte');
+  runner.addCommand(CountBitsCommand());
 
-    if(number == null || number.trim().isEmpty) {
-      print(invalidArgumentMessage);
-      exit(0);
-    }
-
-    if (int.tryParse(number) == null || int.parse(number) < 0 || int.parse(number) > 255) {
-      print(invalidArgumentMessage);
-      exit(0);
-    }
-
-    BitCounter bitCounter = BitCounter();
-    bitCounter.performingTheCalculation(number);
-
-  } else if (arguments[0] == '-h') {
-    print(helpMessage);
-    exit(0);
-  } else {
-    String inputNumber = arguments[0];
-    BitCounter bitCounter = BitCounter();
-    bitCounter.performingTheCalculation(inputNumber);
+  try {
+    runner.run(arguments);
+  } on UsageException catch (e) {
+    print(e.message);
+    print(e.usage);
   }
 }
+
